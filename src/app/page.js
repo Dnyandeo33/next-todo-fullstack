5d5f89db513c48034138caf29a45aac1c9aa107d
Dnyandeo Waghunde <dnyanu.waghunde@gmail.com>
2024-08-20 17:24:07 +0200

    update title

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import ListTodo from "./components/ListTodo";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center w-2/3 p-4 mx-auto mt-8 bg-blue-100">
+    <main className="flex flex-col items-center justify-center w-2/3 p-4 mx-auto mt-8 bg-blue-200 rounded-md">
       <Header />
       <div className="flex flex-col w-full gap-5">
         <ListTodo />
