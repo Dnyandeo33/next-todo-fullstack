79b719b468c910f70ca3896a4ad2427bd79848d3
Dnyandeo Waghunde <dnyanu.waghunde@gmail.com>
2024-08-24 04:26:31 +0200

    updated

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const dbConnect = async () => {
     try {
-        await mongoose.connect(`${process.env.MONGO_URI}/${'todo-list'}`)
+        await mongoose.connect(process.env.MONGO_URI)
         console.log("Connected to MongoDB")
     } catch (error) {
         console.log('mongodb err', error)
